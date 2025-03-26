5f28e0431c2c478ed9dd7ca4adf835e20839aacf
Yvonne-del <ys359375@gmail.com>
2025-03-26 08:45:32 +0300

    hide sections that will show when a button is clicked

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const baseURL = "http://localhost:3000/animals";
+
+function showSection(sectionId) {
+    // Hide all sections
+    document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the selected section
+    document.getElementById(sectionId).style.display = 'block';
+}
